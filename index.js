0ab6d3a05b3ba30a8bcbf65d1bef154a9b834a0f
Omar <omar0940@gmail.com>
2021-08-04 11:39:38 +0200

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const camelCase = () => {};
+const args = process.argv.slice(2);
+//console.log(args);
+
+const result = (str) => {
+  let newArray = str.map((word) => {
+    let fixedWord = word.trim();
+    return fixedWord.charAt(0).toUpperCase() + fixedWord.slice(1).toLowerCase();
+  });
+  console.log(newArray);
+};
+result(args);
